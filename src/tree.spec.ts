447799cff657c12af9a160fcc1461a65150003f8
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-11-14 22:48:31 +1000

    wip

--- src/tree.spec.ts
+++ src/tree.spec.ts
@@ -1,5 +1,5 @@
 import { it, expect, describe } from "vitest";
-import { m } from "vitest/dist/index-2f5b6168";
+import { m, r } from "vitest/dist/index-2f5b6168";
 import { aC } from "vitest/dist/types-f302dae9";
 
 interface Spec {
@@ -24,7 +24,7 @@ function createSpec(p: string, name: string): Spec {
     fileName: name,
     specFileExtension: ".cy.ts",
     fileExtension: ".ts",
-    relative: `${p}/${name}.cy.ts`,
+    relative: p.length > 0 ? `${p}/${name}.cy.ts` : `${name}.cy.ts`,
   };
 }
 
@@ -60,14 +60,18 @@ const defaults: SpecListOptions = {
 
 type DirectoryMap = Map<
   string,
-  { node: SpecTreeDirectoryNode; children: SpecTreeNode[] }
+  { node: SpecTreeDirectoryNode; }
 >;
+
+function isRootLevelSpec (path: string, sep: string) {
+  return !path.includes(sep)
+}
 
 function getDirectories (specs: Spec[], root: SpecTreeDirectoryNode, sep = '/'): DirectoryMap {
   const map: DirectoryMap = new Map()
   const dirNodes: Map<string, SpecTreeDirectoryNode> = new Map()
 
-  map.set('/', { node: root, children: [] })
+  map.set('/', { node: root, })
   dirNodes.set('/', root)
 
   for (const spec of specs) {
@@ -75,6 +79,11 @@ function getDirectories (specs: Spec[], root: SpecTreeDirectoryNode, sep = '/'):
 
     const acc: string[] = []
     const parts = path.split(sep)
+
+    if (isRootLevelSpec(path, sep)) {
+      // root level spec
+      continue
+    }
 
     for (let i = 0; i < parts.length; i++) {
       acc.push(parts[i])
@@ -95,15 +104,64 @@ function getDirectories (specs: Spec[], root: SpecTreeDirectoryNode, sep = '/'):
       }
 
       dirNodes.set(dirName, node)
-      map.set(acc.join(sep), { node, children: [] })
+      map.set(acc.join(sep), { node })
     }
   }
+
+
+  function recursivelyAssignDirectories (node: SpecTreeDirectoryNode) {
+    console.log('assigning children for ', node.name)
+    let i = 0
+
+    if (node.parent) {
+      i++
+      if (i > 50) {
+        throw Error('oikoipakwdfp')
+      }
+      node.parent.children.push(node)
+      recursivelyAssignDirectories(node.parent)
+    }
+  }
+
+  console.log(map.get('cypress')?.node.children)
+  console.log('===========')
+
+  // Next, we assign the children for each directory.
+  console.log(dirNodes.entries())
+  for (const [childName, childNode] of dirNodes.entries()) {
+    recursivelyAssignDirectories(childNode)
+    // const parentName = childNode.parent?.name
+
+    // // Root has no parent.
+    // if (!parentName) {
+    //   continue
+    // }
+
+    // const val = map.get(parentName)!
+    // const allChildren = Array.from(dirNodes.values()).filter(x => x.parent?.name === parentName)
+    // // const children = dirNodes.get(childName)!
+    // map.set(parentName, { 
+    //   node: {
+    //     ...val.node, 
+    //     children: val.node.children.concat(...allChildren) 
+    //   }
+    // })
+
+    // console.log(childNode?.parent?.name, '->', childName)
+    // console.log('children are => ', allChildren)
+    // const node = map.get(name)!
+    // const children = dirNodes.get(node.node.parent)
+    // console.log({ name , node, children })
+    // map.set(name, { node: _ });
+  }
+  console.log('===========')
+  console.log(map.get('cypress')?.node.children)
 
   for (const spec of specs) {
     let { name, path } = splitIntoParts(spec.relative, sep)
     path  ||= '/'
 
-    const parent = map.get(path)
+    const parent = isRootLevelSpec(spec.relative, sep) ? root : map.get(path)?.node
 
     if (!parent) {
       throw Error(`Could not find directory node with key '${path}'. This should never happen.`)
@@ -114,10 +172,10 @@ function getDirectories (specs: Spec[], root: SpecTreeDirectoryNode, sep = '/'):
       type: 'file',
       data: spec,
       directory: path,
-      parent: parent.node
+      parent
     }
 
-    map.get(path)!.node.children.push(fileNode)
+    parent.children.push(fileNode)
   }
 
   return map
@@ -127,8 +185,8 @@ function splitIntoParts(
   path: string,
   sep: string
 ): { name: string; path: string } {
-  if (!path.includes(sep)) {
-    return { name: "/", path: "/" };
+    if (!path.includes(sep)) {
+    return { name: path, path };
   }
 
   const idx = path.lastIndexOf(sep);
@@ -168,6 +226,7 @@ function deriveSpecTree(
 
 
   const directories = getDirectories(specs, root, '/')
+  // console.log({directories})
 
   const rootNode = directories.get('/')?.node
 
@@ -298,12 +357,21 @@ describe("", () => {
     // const actual = deriveSpecTree([s3, s4, s1, s2]);
     const actual = deriveSpecTree([s0, s3, s4]);
     const root = actual.root
-console.log(actual.map.get('/')?.children)
+    // console.log(root.children[0].children)
+// console.log(actual.map.get('/')?.node.children)
 
     expect(root.name).to.eq('/')
     expect(root.type).to.eq('directory')
     expect(root.parent).to.eq(null)
     expect(root.collapsed).to.eq(false)
-    expect(root.children).to.have.length(1)
+    // console.log('chilrdren!!', root.children)
+    // expect(root.children).to.have.length(2)
+
+    const rootDirectorySpec = root.children[0] as SpecTreeFileNode
+    // expect(rootDirectorySpec.name).to.eq('smoke.cy.ts')
+    // expect(rootDirectorySpec.type).to.eq('file')
+    // expect(rootDirectorySpec.data).to.eq(s0)
+    // expect(rootDirectorySpec.parent.name).to.eq('/')
+
   });
 });
